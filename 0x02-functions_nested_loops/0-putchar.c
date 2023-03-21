65c33950dcda6cf0b61033d02a87ed8d4e012454
Yasmeen hany <yasmeenhany14@gmail.com>
2023-03-21 10:59:06 -0700

    fixed loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,7 +13,7 @@ int main(void)
 	int i = 0;
 	char arr[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	
-	for (int i = 0; i < 9; i++)
+	for (; i < 9; i++)
 		_putchar(arr[i]);
 	_putchar('\n');
 	return (0);
