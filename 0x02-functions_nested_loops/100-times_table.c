fb943e783ec3ec0dd2efb791e6e4c1647efceccb
Yasmeen hany <yasmeenhany14@gmail.com>
2023-03-21 16:22:27 -0700

    de

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_num - print number recursivly
+ * @n: number to print
+ *
+ * Return: void
+ */
+void print_num(int n)
+{
+	if (n == 0)
+		return;
+	print_num(n / 10);
+	_putchar(n % 10);
+}
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: start
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			k = i * j;
+			print_num(k);
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
