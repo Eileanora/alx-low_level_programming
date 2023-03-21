b54a2354abba9955b065334b891d199316360a8c
Yasmeen hany <yasmeenhany14@gmail.com>
2023-03-21 10:15:18 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * i: numbers counter
+ * c: characters counter
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
