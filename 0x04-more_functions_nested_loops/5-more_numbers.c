d1239e2813a8603a8f5befbef5f7675fd04629e0
Yasmeen hany <yasmeenhany14@gmail.com>
2023-03-23 17:12:05 -0700

    fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,8 +13,13 @@ void more_numbers(void)
 
 	for (j = 0; j <= 10; j++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (i = 0; i < 10; i++)
 			_putchar(i + '0');
+		for (; i <= 14; i++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
 		_putchar('\n');
 	}
 	_putchar('\n');
