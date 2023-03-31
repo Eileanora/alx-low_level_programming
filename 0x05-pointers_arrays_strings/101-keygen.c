d04ad2c77de46d75d74424acde9292c0039ade8f
Yasmeen hany <yasmeenhany14@gmail.com>
2023-03-31 13:44:44 -0700

    last tasks

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+/**
+ * main - program to generate random valid passwords
+ * len: length of string
+ * str: string of characters
+ * i: counter
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
+        char str [] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"};
+        int len = strlen(str);
+        int i;
+
+        srand(time(NULL));
+        for (i = 0; i < 10; i++)
+        {
+                printf("%c", str[rand() % (len - 1)]);
+        }
+        printf("\n");
+}
