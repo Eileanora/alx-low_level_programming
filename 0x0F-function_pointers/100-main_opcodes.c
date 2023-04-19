9d019fccc57fdf63a16aa8777e65f6e18824e8b7
yasmeen <yasmeenhany14@gmail.com>
2023-04-19 23:04:41 +0200

    advanced

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - prints the opcodes of its own main function
@@ -16,12 +17,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(1);
 	}
-	if (atoi(argv[1]) < 0)
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	bytes = atoi(argv[1]);
 	p = (char *)main;
 	for (i = 0; i < bytes; i++)
 	{
@@ -29,5 +30,6 @@ int main(int argc, char *argv[])
 		if (i < bytes - 1)
 			printf(" ");
 	}
+	printf("\n");
 	return (0);
 }
