7d79b7ce5433825371c9b1aea8a9c3a572d92535
yasmeen <yasmeenhany14@gmail.com>
2023-10-11 15:15:16 +0300

    made it recursive

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -18,6 +18,27 @@ void print_array(size_t l, size_t r, int *array)
 	}
 }
 /**
+ * lower_bound - searches for the first occurance
+ * @l: left index
+ * @r: right index
+ * @array: array to search in
+ * @value: value to search for
+ * Return: index where value is located, otherwise -1
+*/
+int lower_bound(size_t l, size_t r, int *array, int value)
+{
+	size_t mid;
+
+	mid = l + (r - l) / 2;
+	if (l == r - 1)
+		return (array[l] == value ? (int)l : -1);
+	print_array(l, r - 1, array);
+	if (array[mid] >= value)
+		return (lower_bound(l, mid, array, value));
+	else
+		return (lower_bound(mid, r, array, value));
+}
+/**
  * advanced_binary - searches for the first occurance
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array
@@ -26,19 +47,9 @@ void print_array(size_t l, size_t r, int *array)
 */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t l = 0, r = size, mid;
+	size_t l = 0, r = size;
 
-	while (l < r - 1)
-	{
-		print_array(l, r - 1, array);
-		mid = l + (r - l) / 2;
-		if (array[mid] >= value)
-			r = mid;
-		else
-			l = mid;
-	}
-	if (array[l] == value)
-		return (l);
-	else
+	if (array == NULL)
 		return (-1);
+	return (lower_bound(l, r, array, value));
 }
